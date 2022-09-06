602d13210791df6fc14757249c78b660b5e50189
Chris Asher <hi@chrisasher.com>
2022-09-06 14:01:42 +1000

    wp set up init

--- wp-config.php
+++ wp-config.php
@@ -80,6 +80,7 @@ $table_prefix = 'cp_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 define( 'WP_DEBUG', false );
+ini_set('display_errors', 'none');
 
 /* Add any custom values between this line and the "stop editing" line. */
 
