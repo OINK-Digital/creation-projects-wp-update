1ef11057caa8a4846efae35db91b4e84d946d920
Chris Asher <hi@chrisasher.com>
2022-09-06 16:10:45 +1000

    information architecture around pages and projects done. need to add theme settings

--- wp-content/themes/oink-theme/functions/extras.php
+++ wp-content/themes/oink-theme/functions/extras.php
@@ -9,3 +9,16 @@ function my_acf_google_map_api( $api ){
 }
 
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+
+function klf_acf_input_admin_footer() { ?>
+    <script type="text/javascript">
+        (function($) {
+            acf.addFilter('color_picker_args', function (args) {
+                var colors = ['#262838','#7F8C9B','#AF4D2D','#F6F7F7','#E3DFD7','#A7A49E']
+                args.palettes = colors;
+                return args;
+            });
+        })(jQuery);
+    </script>
+<?php }
+add_action('acf/input/admin_footer', 'klf_acf_input_admin_footer');
